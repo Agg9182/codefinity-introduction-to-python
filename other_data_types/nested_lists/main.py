6e62dc653a8438db2e4821976eed58f75bae3bbf
Agg9182 <>
2025-12-13 14:50:42 +0000

    Nested Lists

--- other_data_types/nested_lists/main.py
+++ other_data_types/nested_lists/main.py
@@ -4,3 +4,7 @@ vegetables.append("carrots")
 vegetables.append("cucumbers")
 vegetables.sort()
 print("Updated Vegetable Inventory:", vegetables)
+if "carrots" in vegetables:
+ print("Carrots are already in the list.")
+if "ccucumbers" in vegetables:
+ print("Carrots are already in the list.")
