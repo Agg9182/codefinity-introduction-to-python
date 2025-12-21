c07194aff9d92c1df5a602be67e9b9fc8f2a0c6d
Agg9182 <aldo.gonzalez18@gmail.com>
2025-12-21 14:10:18 -0600

    nested

---
+++ loops/for_loops/order.py
@@ -0,0 +1,9 @@
+word = "program"
+sorted_string = "".join(sorted(word))
+print("Sorted string", sorted_string)# Write your code here
+
+for letter in word :
+    print(letter)
+
+for sorted_letter in sorted_string :
+    print(sorted_letter)
