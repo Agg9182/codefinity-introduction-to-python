55f7ba3f733cd24330a01ff1133b9998c153a2e9
Agg9182 <>
2025-12-21 19:56:02 +0000

    For Loops

---
+++ loops/for_loops/main.py
@@ -0,0 +1,9 @@
+prices = [12.99, 8.50, 15.75, 23.00, 7.25]
+#word = "program"
+#sorted_string = "".join(sorted(word))
+#print("Sorted string", sorted_string)# Write your code here
+
+total = 0
+for price in prices : 
+    total = total + price
+print("total", total)
