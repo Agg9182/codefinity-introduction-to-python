cbdb74f9ab0c4d84958decf38da6ffc5a77e7d46
Agg9182 <>
2025-12-21 23:06:31 +0000

    While Loops

---
+++ loops/while_loops/main.py
@@ -0,0 +1,7 @@
+start_number = 5
+countdown_values = []
+
+while start_number >= 1 :
+ countdown_values.append(start_number)
+ print("Discount countdown complete!",countdown_values)
+ start_number -= 1
