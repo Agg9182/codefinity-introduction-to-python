1d1f3648a7471cf176cf945b420fe1e5ab0afe0c
Agg9182 <>
2025-12-22 18:10:58 +0000

    Range Function

--- loops/range_function/main.py
+++ loops/range_function/main.py
@@ -1,11 +1,9 @@
-# Schedule staff shifts every three hours throughout a 12-hour day
-#for hour in range(1, 13, 3):
-   # print(f"Staff shift starts at hour {hour}.")
-
-
-
 # List of products on promotion for each weekday
-daily_promotions = ["Milk", "Eggs", "Bread", "Apples", "Oranges"]
-
+daily_promotions = ["milk", "eggs", "bread", "apples", "oranges"]
 # List of weekdays corresponding to the promotions
-weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+weekdays = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+
+for day in range(5):
+ weekdays1 = weekdays[day]
+ promos = daily_promotions[day]
+ print(f"{weekdays1}: promotion on {promos}")
